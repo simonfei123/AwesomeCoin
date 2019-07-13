6b75c0b530695b30ed4d8dbab622cf83798fa22e
Simon Fei <simonfei45@gmail.com>
2019-07-13 15:47:20 -0700

    3. Delegated Transfer

--- contracts/AwesomeCoin.sol
+++ contracts/AwesomeCoin.sol
@@ -6,9 +6,15 @@ contract AwesomeCoin {
 	string  public standard = "Awesome Coin v1.0";
 	uint256 public totalSupply;
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 	event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -27,4 +33,28 @@ contract AwesomeCoin {
 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+
 }
