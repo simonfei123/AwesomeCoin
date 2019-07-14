c1909e32f9a070d9a5e4546b59aee449087786b0
Simon Fei <simonfei45@gmail.com>
2019-07-14 14:44:39 -0700

    6. Deploy Contracts

--- truffle-config.js
+++ truffle-config.js
@@ -42,11 +42,18 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-     development: {
+    development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
+    }
 
     // Another network with more advanced options...
     // advanced: {
